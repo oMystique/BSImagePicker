ff4bbfa4d1f03ebb7014a7e9cb35c2696e71ad12
Alexey Ildarkin <ildarkin.alexey@gmail.com>
2019-05-20 00:09:11 +0300

    Added video fetching

--- Source/Model/AlbumTableViewDataSource.swift
+++ Source/Model/AlbumTableViewDataSource.swift
@@ -61,7 +61,7 @@ final class AlbumTableViewDataSource : NSObject, UITableViewDataSource {
         fetchOptions.sortDescriptors = [
             NSSortDescriptor(key: "creationDate", ascending: false)
         ]
-        fetchOptions.predicate = NSPredicate(format: "mediaType = %d", PHAssetMediaType.image.rawValue)
+//        fetchOptions.predicate = NSPredicate(format: "mediaType = %d", PHAssetMediaType.image.rawValue)
         
         let result = PHAsset.fetchAssets(in: album, options: fetchOptions)
         result.enumerateObjects({ (asset, idx, stop) in
